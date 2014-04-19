2f45e30319da90d4f68a85a4526dbb0682844e56
Liam Morley <liam@carpeliam.com>
2014-04-19 12:37:57 -0400

    add start on new homepage
    This is just an initial stab at the markup; the design is still in flux, so this shouldn't be considered blessed code. The inline styles for example won't be around for long.

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -3,10 +3,12 @@ App.Router.map(function() {
   this.resource('projects', function() {
     this.route('show', {path: '/:project_id'});
   });
-  this.resource('events', function() {
-    this.route('show', {path: '/:event_id'});
-  });
   this.resource('members', function() {
     this.route('show', {path: '/:member_id'});
   });
+  this.resource('events', function() {
+    this.route('show', {path: '/:event_id'});
+  });
+  this.route('connect');
+  this.route('resources');
 });
