dd369bb0168a08ea876f3e6d3387423b3f2ee353
Teresa Murphy-Skvorzova <teresa.murphy.skvorzova@gmail.com>
2014-05-06 21:11:33 -0400

    First attempt at improving statistics and getting correct Github details

--- app/scripts/models/project.js
+++ app/scripts/models/project.js
@@ -12,7 +12,20 @@ App.Project = DS.Model.extend({
   pressClippings: DS.attr(),
   featured: DS.attr(),
   githubDetails: DS.attr(),
-
+  commitCount: function () {
+    var details = this.getWithDefault('githubDetails', {});
+    var participation = details.participation;
+    var sum = 0;
+    if (participation == null) {
+      return sum
+    }
+    else {
+      for (i=0;i<participation.length;i++)
+        {sum = sum + participation[i]
+      };
+    }; 
+    return sum
+}.property('githubDetails'),
   background: function() {
     if (this.getWithDefault('screenshots', []).length) {
       return 'background-image: url(' + this.get('screenshots')[0] + ')'
