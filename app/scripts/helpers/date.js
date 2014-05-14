ec2213881d5b8924e991600540983e51f8c2abe3
Teresa Murphy-Skvorzova <teresa.murphy.skvorzova@gmail.com>
2014-05-13 20:13:55 -0400

    timeago function added for project main page

--- app/scripts/helpers/date.js
+++ app/scripts/helpers/date.js
@@ -1,3 +1,8 @@
 Ember.Handlebars.registerBoundHelper('date', function(date, format) {
   return moment(date).format(format);
 });
+
+
+Ember.Handlebars.registerBoundHelper('timeAgo', function(date) {
+  return moment(date).fromNow();
+});
