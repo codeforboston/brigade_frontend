5d13f70d94e849aea46ee69dd2dbddd509d95831
Liam Morley <liam@carpeliam.com>
2014-06-10 16:00:45 -0400

    update project index to more closely conform to evolving design

--- app/scripts/models/project.js
+++ app/scripts/models/project.js
@@ -4,7 +4,8 @@ App.Project = DS.Model.extend({
   codeUrl: DS.attr(),
   history: DS.attr(),
   linkUrl: DS.attr(),
-  projectMembers: DS.hasMany('project-member', {async: true}),
+  // TODO use relationship when available in API
+  // projectMembers: DS.hasMany('project-member', {async: true}),
   status: DS.attr(),
   skillsNeeded: DS.attr(),
   projectPlan: DS.attr(),
@@ -13,7 +14,7 @@ App.Project = DS.Model.extend({
   featured: DS.attr(),
   githubDetails: DS.attr(),
   commitCount: function () {
-    var participation = this.getWithDefault('githubDetails', {}).participation;
+    var participation = this.get('githubDetails.participation');
     var count = 0;
     if (participation) {
       for (var i = 0; i < participation.length; i++) {
@@ -22,9 +23,18 @@ App.Project = DS.Model.extend({
     }
     return count;
   }.property('githubDetails'),
+  // TODO move this into the controller
   background: function() {
     if (this.getWithDefault('screenshots', []).length) {
       return 'background-image: url(' + this.get('screenshots')[0] + ')';
     }
-  }.property('screenshots')
+  }.property('screenshots'),
+
+  // TODO use relationship when available in API
+  projectMembers: function() {
+    var self = this;
+    return this.getWithDefault('githubDetails.contributors', []).map(function(item, i, enumerable) {
+      return Ember.Object.create({project: self, member: App.Member.create(item)});
+    });
+  }.property('githubDetails.contributors')
 });
