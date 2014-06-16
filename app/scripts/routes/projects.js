8cac1091f61684d7c66fe019e0cb92c0aa52116c
Liam Morley <liam@carpeliam.com>
2014-06-15 21:00:59 -0400

    create "share on twitter" project action

--- app/scripts/routes/projects.js
+++ app/scripts/routes/projects.js
@@ -7,5 +7,16 @@ App.ProjectsIndexRoute = Ember.Route.extend(App.ResetScroll, {
 App.ProjectsShowRoute = Ember.Route.extend({
   model: function(params) {
     return this.get('store').find('project', params.project_id);
+  },
+  actions: {
+    shareOnTwitter: function() {
+      var url = encodeURIComponent(window.location.href),
+          via = 'CodeForBoston',
+          text = encodeURIComponent(document.title + ' - ' + this.controller.get('name')),
+          width = 550, height = 420;
+      window.open('https://twitter.com/share?url=' + url + '&via=' + via + '&text=' + text,
+                  'Share a link on Twitter',
+                  'width=' + width + ',height=' + height + ',left=' + (screen.width - width) / 2 + ',top=' + (screen.height - height) / 2);
+    }
   }
 });
