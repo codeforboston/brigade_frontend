aeeeec2c90bdaea1eddd1db8baf4af0d5b3ca07e
Liam Morley <liam@carpeliam.com>
2014-06-23 19:34:28 -0400

    add pagination to bottom of projects page
    This makes use of pagination as defined in the [CfA API](http://codeforamerica.org/api/)

---
+++ app/scripts/controllers/projects.js
@@ -0,0 +1,26 @@
+App.ProjectsIndexController = Ember.ArrayController.extend({
+  queryParams: ['page'],
+  page: 1,
+  previousPage: function() {
+    return this.get('page') - 1;
+  }.property('page'),
+  hasPreviousPage: function() {
+    return this.get('page') > 1;
+  }.property('page'),
+  nextPage: function() {
+    return this.get('page') + 1;
+  }.property('page'),
+  hasNextPage: function() {
+    return this.get('page') < this.get('lastPage');
+  }.property('page', 'lastPage'),
+  hasMultiplePages: function() {
+    return this.get('lastPage') > 1;
+  }.property('lastPage'),
+  availablePages: function() {
+    var pages = [];
+    for (var i = 1, lastPage = this.get('lastPage'); i <= lastPage; i++) {
+      pages.push(i);
+    }
+    return pages;
+  }.property('lastPage')
+});
